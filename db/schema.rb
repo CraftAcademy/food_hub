4215e70319e8d149a1f6a619ab459761e3809711
nader <alghazu@gmail.com>
2018-05-10 13:01:21 +0200

    Fix db migration after merge

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,8 @@ ActiveRecord::Schema.define(version: 2018_05_09_220252) do
     t.text "directions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,7 +39,6 @@ ActiveRecord::Schema.define(version: 2018_05_09_220252) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
