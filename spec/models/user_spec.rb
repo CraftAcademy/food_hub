321a3b84a5bda59860ad5a86aa286dec200c7f7c
nader <alghazu@gmail.com>
2018-05-10 15:17:11 +0200

    Add factoryBot for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  it { is_expected.to have_db_column :email }
-  it { is_expected.to have_db_column :encrypted_password }
+  describe 'Database table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
+
+  describe 'Factory' do
+    it 'can create a valid instance' do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+  end
 end
