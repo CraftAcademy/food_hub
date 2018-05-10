fcc2910ae1bebc7c7e5e19ab3768a008fad1166f
nader <alghazu@gmail.com>
2018-05-10 15:34:57 +0200

    Add Rspec validations for recipe

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
+  recipe = FactoryBot.create(:recipe)
+
   describe 'Database table' do
     it {is_expected.to have_db_column :title}
     it {is_expected.to have_db_column :description}
@@ -10,7 +12,26 @@ RSpec.describe Recipe, type: :model do
 
   describe 'Factory' do
     it 'can create a valid instance' do
-      expect(FactoryBot.create(:recipe)).to be_valid
+      expect(recipe).to be_valid
+    end
+  end
+
+  describe "Validations" do
+    it "is not valid without a title" do
+      recipe.title = nil
+      expect(recipe).to_not be_valid
+    end
+    it "is not valid without a description" do
+      recipe.description = nil
+      expect(recipe).to_not be_valid
+    end
+    it "is not valid without a ingredients" do
+      recipe.ingredients = nil
+      expect(recipe).to_not be_valid
+    end
+    it "is not valid without a directions" do
+      recipe.directions = nil
+      expect(recipe).to_not be_valid
     end
   end
 end
