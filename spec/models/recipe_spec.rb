7312619651e090fa0fa84c8e720e9e47fd7b2ec1
nader <alghazu@gmail.com>
2018-05-10 20:05:36 +0200

    Change rspec validation to shoulda matcher

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,33 +5,18 @@ RSpec.describe Recipe, type: :model do
 
   describe 'Database table' do
     it {is_expected.to have_db_column :title}
+    it { should validate_presence_of(:title) }
     it {is_expected.to have_db_column :description}
+    it { should validate_presence_of(:description) }
     it {is_expected.to have_db_column :ingredients}
+    it { should validate_presence_of(:ingredients) }
     it {is_expected.to have_db_column :directions}
+    it { should validate_presence_of(:directions) }
   end
 
   describe 'Factory' do
     it 'can create a valid instance' do
       expect(recipe).to be_valid
-    end
-  end
-
-  describe "Validations" do
-    it "is not valid without a title" do
-      recipe.title = nil
-      expect(recipe).to_not be_valid
-    end
-    it "is not valid without a description" do
-      recipe.description = nil
-      expect(recipe).to_not be_valid
-    end
-    it "is not valid without a ingredients" do
-      recipe.ingredients = nil
-      expect(recipe).to_not be_valid
-    end
-    it "is not valid without a directions" do
-      recipe.directions = nil
-      expect(recipe).to_not be_valid
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,23 +5,14 @@ RSpec.describe User, type: :model do
   user = FactoryBot.create(:user)
   describe 'Database table' do
     it { is_expected.to have_db_column :email }
+    it { should validate_presence_of(:email) }
     it { is_expected.to have_db_column :encrypted_password }
+    it { should validate_presence_of(:password) }
   end
 
   describe 'Factory' do
     it 'can create a valid instance' do
       expect(user).to be_valid
-    end
-  end
-
-  describe "Validations" do
-    it "is not valid without a email" do
-      user.email = nil
-      expect(user).to_not be_valid
-    end
-    it "is not valid without a password" do
-      user.password = nil
-      expect(user).to_not be_valid
     end
   end
 end
