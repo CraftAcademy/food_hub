d5d42d9ac1765b1516f6ecc97bb89889ee8dbec1
nader <alghazu@gmail.com>
2018-05-12 15:16:05 +0200

    Modify Rspec test for recipe

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -1,8 +1,9 @@
-FactoryBot.define do
+ FactoryBot.define do
   factory :recipe do
     title 'MyRecipe'
     description 'A fake recipe to try cook something'
     ingredients 'Eggs, flour, tomato, meat, other stuff'
     directions 'Put stuff together and mix them, then bake them'
+    user User.create(email: 'maran@dream.com', password: '123456')
   end
 end
