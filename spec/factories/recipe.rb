1ef0e4cf56d4f1b1c4c0d2ee498c282317d7985a
tochman <thomas@craftacademy.se>
2018-05-12 19:16:53 +0200

    fixes recipe accissiation to user in factory

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -4,6 +4,6 @@
     description 'A fake recipe to try cook something'
     ingredients 'Eggs, flour, tomato, meat, other stuff'
     directions 'Put stuff together and mix them, then bake them'
-  #  user User.create(email: 'maran@dream.com', password: '123456')
+    user
   end
 end
