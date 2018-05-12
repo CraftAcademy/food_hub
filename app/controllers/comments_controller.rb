3a716269ab81d9df3c8e963609fd5da801e2cb81
petra_bodman <petra_bodman@hotmail.com>
2018-05-12 11:46:13 +0200

    Modify comment form, adds comments model, display comments recipe on show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    Comment.create(body: params[:comment][:body], recipe_id: recipe.id, user: current_user)
+    flash[:notice] = 'Thank you for your feedback'
+    redirect_to recipe_path(recipe)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :comments
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: {minimum: 3}
   validates :description, presence: true
   validates :ingredients, presence: true
   validates :directions, presence: true
