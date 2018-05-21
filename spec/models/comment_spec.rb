c9796eea7f63a1c00b1fe70ac92fe38fe3c20e69
tochman <thomas@craftacademy.se>
2018-05-21 13:14:46 +0200

    fixes failing comments spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  let(:user) {craete(:user)}
   it {is_expected.to have_db_column :body}
   it {is_expected.to belong_to :user}
   it {is_expected.to belong_to :recipe}
 
   it 'has a valid factory' do
-    expect(FactoryBot.create(:comment)).to be_valid
+    expect(FactoryBot.create(:comment, user: user)).to be_valid
   end
 end
