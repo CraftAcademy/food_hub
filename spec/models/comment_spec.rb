77bc1b8dd07cc6a419765f8e0efa21ee039a4c9e
George_Lewis <georgejlewis@hotmail.com>
2018-06-12 16:49:12 +0200

    WIP troubleshooting

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Comment, type: :model do
   end
 
   describe 'Broadcast notification' do
-    it 'on create' do 
+    it 'on create' do
       expect{ create(:comment, user: user, recipe: recipe)}
       .to have_broadcasted_to('notifications')
       .from_channel(WebNotificationsChannel)
       .with({message: '<p>whatever@random.com left a comment on Pizza.</p>'})
-    end 
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,12 +7,16 @@ require "action_cable/testing/rspec"
 
 ActiveRecord::Migration.maintain_test_schema!
 
+ENV['RSPEC'] = 'true'
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
+  config.add_setting :adapter
+  config.adapter = 'test'
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
