717c68615525e4fc11c673228f7b3330476c6134
tochman <thomas@craftacademy.se>
2018-06-13 12:10:16 +0200

    adds image display on recipe#index

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -22,10 +22,18 @@ end
 
 Given('We have the following recipes:') do |table|
   table.hashes.each do |recipe|
-    if recipe[:user]
+    if recipe[:user] && recipe[:image]
+      user = User.find_by email: recipe[:user]
+      recipe_hash = recipe.except('user', 'image')
+      new_recipe = create(:recipe, recipe_hash.merge(user: user))
+      new_recipe.image.attach(io: File.open("#{::Rails.root}/spec/fixtures/#{recipe[:image]}"), 
+                              filename: "#{recipe[:image]}",
+                              content_type: "image/png")
+    elsif recipe[:user]
       user = User.find_by email: recipe[:user]
       recipe = recipe.except('user')
       create(:recipe, recipe.merge(user: user))
+   
     else
       create(:recipe, recipe)
     end
