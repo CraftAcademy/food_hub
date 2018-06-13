1a5fff6c4a4fe4093269edb75677843dc5fbff64
tochman <thomas@craftacademy.se>
2018-06-13 10:12:36 +0200

    fixes adapter issue for AC in RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+ENV['TEST_FRAMEWORK'] = 'rspec'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
