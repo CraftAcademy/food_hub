292456dc1eb5c0ae846b86584350412baf91f61a
tochman <thomas@craftacademy.se>
2018-06-13 10:15:42 +0200

    cleans out unused code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,17 +8,14 @@ require "action_cable/testing/rspec"
 
 ActiveRecord::Migration.maintain_test_schema!
 
-ENV['RSPEC'] = 'true'
-
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.add_setting :adapter
-  config.adapter = 'test'
 end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
