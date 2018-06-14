3f6d523ada966abf5fd1db443f9fc077b81e100d
magnus-thor <magnus.thor.ar@gmail.com>
2018-06-14 16:59:17 +0200

    skip setting the ES port when running tests

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,6 +1,6 @@
 Chewy.root_strategy = :atomic
 if !ENV['CHEWY'] and ENV['RAILS_ENV'] == 'test'
-    # Chewy.settings = {host: 'localhost:9250'}
+    # we skip setting the port if running tests
 else
     Chewy.settings = {host: 'localhost:9200'}
 end
