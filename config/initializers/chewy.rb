df50072026110b7901d38bed2164233fe66cd90f
magnus-thor <magnus.thor.ar@gmail.com>
2018-06-14 16:52:14 +0200

    fix typo in chewy.rb

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,6 +1,6 @@
 Chewy.root_strategy = :atomic
 if !ENV['CHEWY'] and ENV['RAILS_ENV'] == 'test'
-    Chewy.settings = {host: 'localhost:9200'}
+    # Chewy.settings = {host: 'localhost:9250'}
 else
     Chewy.settings = {host: 'localhost:9200'}
 end
