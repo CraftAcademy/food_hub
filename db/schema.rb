4429bc7c013df7de748e18e02fc65b78d0f57fb4
George_Lewis <georgejlewis@hotmail.com>
2018-06-20 14:26:21 +0200

    fix merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_19_122743) do
+ActiveRecord::Schema.define(version: 2018_06_17_230921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,11 +74,10 @@ ActiveRecord::Schema.define(version: 2018_06_19_122743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "category_id"
+    t.bigint "collection_id"
     t.integer "original_recipe_id"
     t.string "forked_recipes_ids", default: [], array: true
-    t.bigint "category_id"
-    t.integer "rating"
-    t.bigint "collection_id"
     t.index ["category_id"], name: "index_recipes_on_category_id"
     t.index ["collection_id"], name: "index_recipes_on_collection_id"
     t.index ["user_id"], name: "index_recipes_on_user_id"
