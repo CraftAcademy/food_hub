7e9388208f1ef4835f1ae15d814106cb5b35fd4f
George_Lewis <georgejlewis@hotmail.com>
2018-06-21 17:25:48 +0200

    fix merge conflicts

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -14,37 +14,6 @@ When('I fill in {string} with {string}') do |field, text|
   fill_in field, with: text
 end
 
-Given('We have the following user:') do |table|
-  table.hashes.each do |field|
-    create(:user, field)
-  end
-end
-
-Given('We have the following recipes:') do |table|
-  table.hashes.each do |recipe|
-    if recipe[:user]
-      recipe[:user] = User.find_by email: recipe[:user]
-    end
-
-    if recipe["forked from"]
-      recipe[:original_recipe_id] = Recipe.find_by(title: recipe["forked from"]).id if recipe["forked from"] != ""
-      recipe.delete("forked from")
-    end
-
-    if recipe[:category]
-      recipe[:category] = Category.find_by name: recipe[:category]
-    end
-
-    new_recipe = create(:recipe, recipe.except('image'))
-
-    if recipe[:image]
-      new_recipe.image.attach(io: File.open("#{::Rails.root}/spec/fixtures/#{recipe[:image]}"),
-                              filename: "#{recipe[:image]}",
-                              content_type: "image/png")
-    end
-  end
-end
-
 Given("I am logged in as {string}") do |user_email|
   @user =  user = User.find_by(email: user_email) || create(:user, email: user_email)
   login_as @user, scope: :user
--- features/step_definitions/new_objects.rb
+++ features/step_definitions/new_objects.rb
@@ -6,23 +6,25 @@ end
 
 Given('We have the following recipes:') do |table|
   table.hashes.each do |recipe|
-    if recipe[:user] && recipe[:image]
-      user = User.find_by email: recipe[:user]
-      recipe_hash = recipe.except('user', 'image')
-      new_recipe = create(:recipe, recipe_hash.merge(user: user))
+    if recipe[:user]
+      recipe[:user] = User.find_by email: recipe[:user]
+    end
+
+    if recipe["forked from"]
+      recipe[:original_recipe_id] = Recipe.find_by(title: recipe["forked from"]).id if recipe["forked from"] != ""
+      recipe.delete("forked from")
+    end
+
+    if recipe[:category]
+      recipe[:category] = Category.find_by name: recipe[:category]
+    end
+
+    new_recipe = create(:recipe, recipe.except('image'))
+
+    if recipe[:image]
       new_recipe.image.attach(io: File.open("#{::Rails.root}/spec/fixtures/#{recipe[:image]}"),
                               filename: "#{recipe[:image]}",
                               content_type: "image/png")
-    elsif recipe[:user]
-      user = User.find_by email: recipe[:user]
-      recipe = recipe.except('user')
-      create(:recipe, recipe.merge(user: user))
-    elsif recipe[:category]
-      category = Category.find_by name: recipe[:category]
-      recipe = recipe.except('category')
-      create(:recipe, recipe.merge(category: category))
-    else
-      create(:recipe, recipe)
     end
   end
 end
