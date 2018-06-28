9a2d6d275d0561a31fabd3c555834fbeef65241a
tochman <thomas@craftacademy.se>
2018-06-28 10:24:14 +0200

    refactors the times extension and extracts into new file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-var timesFunction = function(callback) {
-    if (typeof callback !== "function" ) {
-      throw new TypeError("Callback is not a function");
-    } else if( isNaN(parseInt(Number(this.valueOf()))) ) {
-      throw new TypeError("Object is not a valid number");
-    }
-    for (var i = 0; i < Number(this.valueOf()); i++) {
-      callback(i);
-    }
-  };
-
-  String.prototype.times = timesFunction;
-  Number.prototype.times = timesFunction;
