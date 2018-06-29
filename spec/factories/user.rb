496f1e312f47f8fb497eec54fb946001ea84e8a0
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-06-29 23:17:25 +0200

    modify user factoryBot

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do |u|
     u.sequence(:email) {|u| "anders#{u}@test.com"}
     u.password '12345678'
-    u.sequence(:full_name) {|u| "Maran Maransdottir"}
+    u.sequence(:full_name) {|u| "Maran#{u} Maransdottir"}
   end
 end
