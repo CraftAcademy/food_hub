d95ebd6da5b25460ff475bee2ff7f937bf2067be
tochman <thomas@craftacademy.se>
2018-06-30 11:49:18 +0200

    fixes custom uploader button functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,21 +15,11 @@
 //= require turbolinks
 //= require_tree .
 document.addEventListener('turbolinks:load', () => {
-  let imageInput = document.querySelector('#recipe_image')
-  imageInput.addEventListener('change', (event) => {
-    event.preventDefault()
-    let files = event.target.files
-    let image = files[0]
-    let reader = new FileReader()
-    reader.onload = function(file) {
-      const image = new Image();
-      img.src = file.target.result;
-      document.querySelector('#preview').innerHTML = image
-    }
-    debugger
-  })
   let element = document.querySelector('div #flashes')
   setTimeout(() => {
     element.style.visibility = 'hidden'
   }, 5000)
+
+  
+
 })
